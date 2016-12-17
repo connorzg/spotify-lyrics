d97079ebb7747a4e79f0b27e63d04021e59ef3ca
Andre Dieb Martins <andre.dieb@gmail.com>
2016-12-17 05:15:40 -0500

    tests: fix binary path per platform build

--- test/test_index.js
+++ test/test_index.js
@@ -1,13 +1,34 @@
-var Application = require('spectron').Application
-var assert = require('assert')
+const path = require('path')
+const Application = require('spectron').Application
+const assert = require('assert')
 
 describe('application launch', function () {
   this.timeout(10000)
 
   beforeEach(function () {
+    let pathToBinary;
+
+    switch (process.platform) {
+      case 'linux':
+        pathToBinary = path.join(__dirname, '../dist/linux-unpacked/lyrics-for-spotify');
+        break;
+
+      case 'darwin':
+        pathToBinary = path.join(__dirname, '../dist/mac/Lyrics\ for\ Spotify.app/Contents/MacOS/Lyrics\ for\ Spotify');
+        break;
+
+      case 'win32':
+        pathToBinary = path.join(__dirname, '../dist/win-unpacked/Lyrics for Spotify.exe');
+        break;
+
+      default:
+        throw new Error('Path to the built binary needs to be defined for this platform in test/index.js');
+    }
+
     this.app = new Application({
-      path: '/Applications/Lyrics\ for\ Spotify.app/Contents/MacOS/Lyrics\ for\ Spotify'
+      path: pathToBinary
     })
+
     return this.app.start()
   })
 
